d8febf9f44c974e36c378fabd4200bda333729e8
regalrr0 <ryan.r.regal@gmail.com>
2019-09-07 17:45:12 -0400

    Made button purecomponent for efficiency

--- src/MenuButton.js
+++ src/MenuButton.js
@@ -1,8 +1,21 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 import './MenuButton.css';
 import { tsParenthesizedType } from "@babel/types";
 
-class MenuButton extends Component {
+// make purecomponent to avoid uneccessary rendering
+class MenuButton extends PureComponent {
+    
+    /*
+    // could make rendering more efficient by:
+    // **** can be updated with useeffect hook ****
+    shouldComponentUpdate(nextProps, nextState) {
+        if (nextProps.handleMouseDown === this.props.handleMouseDown) {
+            return false;
+          } else {
+            return true;
+          }
+      } 
+    */
     render(){
         return (
             <button id="roundButton" onMouseDown={this.props.handleMouseDown} >
